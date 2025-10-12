4e87a22a243c00bf0ae52afc33c9b1ba35b48b13
Duncan Osuka <duncan.osuka@ea.aku.edu>
2025-10-12 22:01:49 +0300

    Add Activity serializer, viewset, and API routing

--- workout_management/views.py
+++ workout_management/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Activity
+from .serializers import ActivitySerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
